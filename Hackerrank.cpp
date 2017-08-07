953375c23e5f2bbd9274296519f3cea350fa0e81
Piyush Pahwa <pahwapiyush03@gmail.com>
2017-08-07 17:22:36 +0530

    1.0

---
+++ Hackerrank.cpp
@@ -0,0 +1,44 @@
+#include <cstdlib>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <list>
+#include <stack>
+#include <set>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <utility>  
+#include <iterator>
+
+using namespace std;
+
+#define tr(x,it)  for( auto it=begin(x); it!=end(x);it++)
+#define loop(a,b,i) for(int i=a;i<b;i++)
+#define rloop(a,b,i) for(int i=b-1;i>=a;i--)
+#define var(x) int x; cin>>x
+#define mp make_pair
+#define pb push_back
+#define pf pop_front
+#define fi first
+#define se second
+#define test() var(t); while(t--)
+#define dsp printf
+
+typedef vector<int> vint;
+typedef list<int> lint;
+typedef long long ll;
+
+int main(){
+
+    return 0;
+}
